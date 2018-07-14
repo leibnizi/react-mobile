b8fdd545b150d127b2f6779c31fdab0afa599cb5
leibnizi <learnwen@gmail.com>
2018-07-15 02:42:48 +0800

    router init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import BottomTab from './routes'
+
 import './styles/App.css';
 
 class App extends Component {
@@ -11,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <BottomTab />
       </div>
     );
   }
