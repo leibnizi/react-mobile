29227ed648e030223286178adce997e80a16668d
wedge <wedgechiang18@gmail.com>
2018-07-18 23:22:10 +0800

    eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BottomTab from './routes'
 
+import 'antd-mobile/dist/antd-mobile.css';
 import './styles/index.less';
 
 class App extends Component {
